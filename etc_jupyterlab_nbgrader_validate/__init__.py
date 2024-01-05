8218e795e745565abde6069ef8e66d0ccc869ac7
Mengyan Wu <mengyanw@umich.edu>
2024-01-05 17:04:39 -0500

    Fix wrong old code

--- etc_jupyterlab_nbgrader_validate/__init__.py
+++ etc_jupyterlab_nbgrader_validate/__init__.py
@@ -9,16 +9,11 @@ subprocess.run(["jupyter", "labextension", "disable", "nbgrader:validate-assignm
 
 HERE = Path(__file__).parent.resolve()
 
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": data["name"]
+        "dest": "@educational-technology-collective/etc_jupyterlab_nbgrader_validate"
     }]
-
-
 
 from .handlers import setup_handlers
 
